7a06216e481f30157b49767b9fff6dff36757ebe
William <William@192.168.1.12>
2017-01-18 22:39:50 +0100

    First commit 18/01

---
+++ src/Question.java
@@ -0,0 +1,18 @@
+
+public abstract class Question {
+	public static final String fin = " ?";
+	private String theme;
+	private String questionAnswer;
+	private int level;
+	
+	public Question(String theme, String questionAnswer, int difficulty){
+		this.questionAnswer=questionAnswer;
+		this.theme=theme;
+		this.level=difficulty;
+	}
+	
+	public String getTheme() {
+		return theme;
+	}
+
+}
